5f36ebc667c51206ff40e56beb34aa24dd1c15d5
Benson Macharia <muchokibenson12@gmail.com>
2019-05-05 19:41:53 +0300

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit {
+
+  pages = [
+    {
+      title: 'About',
+      url: '/tabs/about',
+      icon: 'information-circle'
+    },
+    {
+      title: 'FeedBack',
+      url: '/tabs/feedback',
+      icon: 'logo-twitch'
+    },
+    {
+      title: 'Contact Us',
+      url: '/tabs/contact',
+      icon: 'contacts'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationEnd) {
+        this.pages.map( p => {
+          return p['active'] = (event.url === p.url);
+        });
+      }
+    });
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}

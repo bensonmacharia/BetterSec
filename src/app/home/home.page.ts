89c4fa4fbc88b2bbb7f5ad77fd7cdd8da2c5126b
Benson Macharia <muchokibenson12@gmail.com>
2019-05-06 17:37:43 +0300

    Test Listing Items

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Elearn, ElearnService } from '../services/elearn.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  elearns: Elearn[];
+
+  constructor(private elearnService: ElearnService) { }
+
+  ngOnInit() {
+    this.elearnService.getElearns().subscribe(res => {
+      this.elearns = res;
+    });
+  }
+
+  remove(item) {
+    this.elearnService.removeElearn(item.id);
+  }
 
 }
--- src/app/services/elearn.service.ts
+++ src/app/services/elearn.service.ts
@@ -1,12 +1,47 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ElearnService {
 
-  constructor() { }
+    private elearnsCollection: AngularFirestoreCollection<Elearn>;
+    private elearns: Observable<Elearn[]>;
+
+    constructor(db: AngularFirestore) {
+        this.elearnsCollection = db.collection<Elearn>('elearns');
+
+        this.elearns = this.elearnsCollection.snapshotChanges().pipe(
+            map(actions => {
+                return actions.map(a => {
+                    const data = a.payload.doc.data();
+                    const id = a.payload.doc.id;
+                    return {id, ...data};
+                });
+            })
+        );
+    }
+
+    getElearns() {
+        return this.elearns;
+    }
+
+    getElearn(id) {
+        return this.elearnsCollection.doc<Elearn>(id).valueChanges();
+    }
+
+    removeElearn(id) {
+        return this.elearnsCollection.doc(id).delete();
+    }
+}
+
+export interface Elearn {
+    elaernId?: number;
+    elearnDomainId: number;
+    elearnContent: string;
+    elearnCreatedBy: number;
+    elearnCreatedOn: string;
 }
